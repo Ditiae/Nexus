ef6e778897167e6f1a0fed707356a38fc24d09a5
codemicro <tom@tdpain.net>
2019-11-12 18:26:36 +0000

    API - multiple changes file_id and size_kb can now be -1, 0 or null, adult_content field added, general improvements and fixes.

--- API/README.md
+++ API/README.md
@@ -1,19 +1,36 @@
 # Upload API
 
-Endpoint - https://arch.tdpain.net/api/nexusmod/create/
+#### Endpoint
 
-Params
+https://arch.tdpain.net/api/nexusmod/create/ - Only POST requests are accepted.
 
- * `mod_id` (required and must be int)
+#### Params
+
+ * `mod_id` (required and can be a number with a decimal point)
  * `mod_name` (required)
  * `mod_desc`
  * `mod_version`
- * `file_id` (required and must be int)
- * `size_kb` (required and must be int)
+ * `file_id` (must be integer if specified in request)
+ * `size_kb` (must be integer if specified in request)
  * `category_name`
- * `content_preview` (must be valid JSON)
+ * `adult_content` (must be Boolean if specified in request)
+ * `content_preview` (must be valid JSON if specified in request)
  * `external_virus_scan_url`
- * `uploaded_time` (must be int as timestamp)
+ * `uploaded_time` (must be an integer timestamp if specified in request)
  * `key` (Authentication key, required)
 
-Only POST requests are accepted.
+#### Specifying a parameter as NULL
+
+If a parameter should be NULL, it can be one of the following:
+
+- not specified in the request
+- "null" in any capitalisation
+
+#### Specifying a parameter as a Boolean
+
+If a parameter should be a Boolean, it can be one of the following:
+
+- "1"
+- "0"
+- "true" in any capitalisation
+- "false" in any capitalisation
--- API/public/nexusmod/create/index.php
+++ API/public/nexusmod/create/index.php
@@ -23,15 +23,18 @@ if (!$conn) { // if that connection didn't work
     e("Internal server error", $code=500);
 }
 
-if (empty($_POST["key"])) {
+
+// check if auth key is specified
+if (array_key_exists("key", $_POST["key"])) {
   e("Provide an auth key");
-} elseif (!in_array($_POST["key"], $AUTH_KEYS)) {
+} elseif (!in_array($_POST["key"], $AUTH_KEYS)) {  // check auth key
   e("Invalid auth key", $code=403);
 }
 
-$fields = array("mod_id", "mod_name", "mod_desc", "mod_version", "file_id", "size_kb", "category_name", "content_preview", "uploaded_time", "external_virus_scan_url");
+$fields = array("mod_id", "mod_name", "mod_desc", "mod_version", "file_id", "size_kb", "category_name", "adult_content", "content_preview", "uploaded_time", "external_virus_scan_url");
 $inputs = array();
 
+// valstring and markstring used for SQL query to populate with values
 $valstring = "";
 $markstring = "";
 foreach ($fields as $name) {
@@ -41,18 +44,22 @@ foreach ($fields as $name) {
 $valstring = substr($valstring, 0, -1);  // remove extra comma from each
 $markstring = substr($markstring, 0, -1);
 
-// check no null fields
-$cannot_be_empty = ["mod_id", "mod_name", "file_id", "size_kb"];
+// check required fields
+$cannot_be_empty = ["mod_id", "mod_name"];
 foreach ($cannot_be_empty as $name) {
-  if (empty($_POST[$name])) {
+  if (!array_key_exists($name, $_POST)) {
     e("Failed - specify param {$name} in POST");
   }
 }
 
 // assign values to fields
 foreach ($fields as $name) {
-  if (empty($_POST[$name])) {
+  if ((!array_key_exists($name, $_POST)) || (strtolower($_POST[$name]) == "null")) {
     $inputs[$name] = NULL;
+  } elseif (strtolower($_POST[$name]) == "true") {
+    $inputs[$name] = "1";
+  } elseif (strtolower($_POST[$name]) == "false") {
+    $inputs[$name] = "0";
   } else {
     $inputs[$name] = $_POST[$name];
   }
@@ -61,14 +68,24 @@ foreach ($fields as $name) {
 // check integers
 $fields_with_integers = ["file_id", "size_kb", "uploaded_time"];
 foreach ($fields_with_integers as $name) {
-  if ((!ctype_digit($inputs[$name])) || (strpos($inputs[$name], ".") !== false)) {
-    e("{$name} can only be an integer");
+  if ($inputs[$name] != NULL) {
+    if ((!is_numeric($inputs[$name])) || (strpos($inputs[$name], ".") !== false)) {
+      e("{$name} can only be an integer");
+    }
+  }
+}
+
+// check booleans
+$fields_with_booleans = ["adult_content"];
+foreach ($fields_with_booleans as $name) {
+  if (!(($inputs[$name] === NULL) || (in_array($inputs[$name], array("1", "0"))))) {
+    e("{$name} can only only accept one of the following: 1, 0, true, false or null");  // true/false filtered out when loading POST params
   }
 }
 
 // check number - can have a decimal point
 if ((!ctype_digit(str_replace(".", "", $inputs["mod_id"])))) {
-  e("mod_id can only be numbers");
+  e("mod_id can only be numbers (with a decimal point if required)");
 }
 
 // check json validity
@@ -80,7 +97,12 @@ if (json_last_error() != JSON_ERROR_NONE) {
 $sqlstr = "INSERT INTO skyrim ({$valstring}) VALUES ({$markstring})";
 
 $sql = $conn->prepare($sqlstr);
-$sql->bind_param("ssssssssss", $inputs["mod_id"], $inputs["mod_name"], $inputs["mod_desc"], $inputs["mod_version"], $inputs["file_id"], $inputs["size_kb"], $inputs["category_name"], $inputs["content_preview"], $inputs["uploaded_time"],  $inputs["external_virus_scan_url"]);
+
+if (!$sql) {
+  e("Internal server error", $code=500);
+}
+
+$sql->bind_param("sssssssssss", $inputs["mod_id"], $inputs["mod_name"], $inputs["mod_desc"], $inputs["mod_version"], $inputs["file_id"], $inputs["size_kb"], $inputs["category_name"], $inputs["adult_content"], $inputs["content_preview"], $inputs["uploaded_time"], $inputs["external_virus_scan_url"]);
 
 $sql->execute();
 $sql->close();
