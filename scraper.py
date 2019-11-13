71b462123d6269bc18fa6d3b73dd20b0883198c8
Wealth <30428646+Wea1thRS@users.noreply.github.com>
2019-11-12 21:20:29 -0600

    fix api request sleep

--- scraper.py
+++ scraper.py
@@ -39,7 +39,7 @@ def parse_api_time(date):
 
 
 def waitforapirequests(hourlyreset):
-    delta = parse_api_time(hourlyreset) - datetime.timestamp(datetime.now)
+    delta = parse_api_time(hourlyreset) - datetime.timestamp(datetime.now())
     print(f"Waiting {delta} seconds for api requests to reset...")
     time.sleep(delta)
 
