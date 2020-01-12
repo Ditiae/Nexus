73c287d29070369bd49e339990a611f8529879d8
codemicro <tom@tdpain.net>
2020-01-12 13:48:50 +0000

    Removed chunk of commented code

--- DataScraper/scraper.py
+++ DataScraper/scraper.py
@@ -222,30 +222,3 @@ with logger.catch():
             }
             r = requests.post(API_URL + "create/", data=params)
             print(f"{r.text}")
-
-            # file_dict = {}
-            # for file in c['files']:
-            #     mod_name = file['name']
-            #     mod_description = file['description']
-            #     links = []
-            #     if file['category_id'] < 6:
-            #         file_id = str(file['file_id'])
-            #         r = requests.get(f"https://api.nexusmods.com/v1/games/{GAME}/mods/{mod_id}/files/
-            #         {file_id}/download_"
-            #                          f"link.json", headers=headers)
-            #         c = json.loads(r.content)
-            #         print("starting download")
-            #         url = c[1]['URI']
-            #         print(url)
-            #         with requests.get(url, stream=True) as s:
-            #             s.raise_for_status()
-            #             os.makedirs(os.path.join("mods", mod_name))
-            #             with open(os.path.join("mods", mod_name, file['file_name']), 'wb+') as m:
-            #                 for chunk in s.iter_content(chunk_size=8192):
-            #                     if chunk:  # filter out keep-alive new chunks
-            #                         m.write(chunk)
-            #         print("finished")
-            #         for link in range(len(c)):
-            #             links.append(str(c[link]['URI'].replace(" ", "%20")))
-            #     file_dict[str(file_id)] = tuple([str(mod_name), str(mod_description), links])
-            # mods[str(mod_id)] = file_dict
